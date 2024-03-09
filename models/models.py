03a025e6a2cbb69197e731f62febd29b281dbade
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-09 17:03:47 -0500

    refactor: ModelData class in models.py to include model & model type

--- models/models.py
+++ models/models.py
@@ -1,28 +1,40 @@
-from pydantic import Field, field_validator, BaseModel, validator
+from pydantic import Field, field_validator, BaseModel, ValidationInfo
 import csv
 from typing import Optional, List, Tuple, Union
 import re
 
 
 class ModelData(BaseModel):
-    model_type: str
-    model: str
-    input_per_1m_tokens: Optional[float] = Field(None, alias="Input per 1M tokens")
-    input_per_1k_tokens: Optional[float] = Field(None, alias="Input per 1K tokens")
-    input_per_token: Optional[float] = Field(None, alias="Input per token")
-    output_per_1m_tokens: Optional[float] = Field(None, alias="Output per 1M tokens")
-    output_per_1k_tokens: Optional[float] = Field(None, alias="Output per 1K tokens")
-    output_per_token: Optional[float] = Field(None, alias="Output per token")
-    training_per_1m_tokens: Optional[float] = Field(
+    model_type: str = Field(..., alias="Model Type")
+    model: str = Field(..., alias="Model")
+    input_per_1m_tokens: Optional[float | str] = Field(
+        None, alias="Input per 1M tokens"
+    )
+    input_per_1k_tokens: Optional[float | str] = Field(
+        None, alias="Input per 1K tokens"
+    )
+    input_per_token: Optional[float | str] = Field(None, alias="Input per token")
+    output_per_1m_tokens: Optional[float | str] = Field(
+        None, alias="Output per 1M tokens"
+    )
+    output_per_1k_tokens: Optional[float | str] = Field(
+        None, alias="Output per 1K tokens"
+    )
+    output_per_token: Optional[float | str] = Field(None, alias="Output per token")
+    training_per_1m_tokens: Optional[float | str] = Field(
         None, alias="Training per 1M tokens"
     )
-    training_per_1k_tokens: Optional[float] = Field(
+    training_per_1k_tokens: Optional[float | str] = Field(
         None, alias="Training per 1K tokens"
     )
-    training_per_token: Optional[float] = Field(None, alias="Training per token")
-    usage_per_1m_tokens: Optional[Union[float, str]] = Field(None, alias="Usage per 1M tokens")
-    usage_per_1k_tokens: Optional[Union[float, str]]= Field(None, alias="Usage per 1K tokens")
-    usage_per_token: Optional[Union[float, str]]= Field(None, alias="Usage per token")
+    training_per_token: Optional[float | str] = Field(None, alias="Training per token")
+    usage_per_1m_tokens: Optional[Union[float, str]] = Field(
+        None, alias="Usage per 1M tokens"
+    )
+    usage_per_1k_tokens: Optional[Union[float, str]] = Field(
+        None, alias="Usage per 1K tokens"
+    )
+    usage_per_token: Optional[Union[float, str]] = Field(None, alias="Usage per token")
     usage_unit: Optional[str] = None
 
     @field_validator(
@@ -35,16 +47,18 @@ class ModelData(BaseModel):
         "training_per_1m_tokens",
         "training_per_1k_tokens",
         "training_per_token",
-        pre=True,
     )
+    @classmethod
     def extract_cost_and_unit(cls, v):
-        if v is None or v == "-":
+        # print(v, "v")
+        if v is None or v == "-" or str(v) == "":
             return None
         cost_unit_re = re.compile(r"([\d.]+)\s*/\s*(.*)")
         match = cost_unit_re.match(v)
         if match:
+            print(match.groups()[0], "match.groups()[0]")
             return match.groups()[0]
-        return v
+        return float(v)
 
     @field_validator(
         "usage_per_1m_tokens",
@@ -52,13 +66,14 @@ class ModelData(BaseModel):
         "usage_per_token",
         check_fields=True,
     )
-    def set_usage_unit(cls, v, values, field):
-        if "usage_unit" in values or v is None:
+    @classmethod
+    def set_usage_unit(cls, v, info: ValidationInfo):
+        if "usage_unit" in info.data or v is None:
             return v
         cost_unit_re = re.compile(r"([\d.]+)\s*/\s*(.*)")
         match = cost_unit_re.match(v)
         if match:
-            values["usage_unit"] = match.groups()[1]
+            info.data["usage_unit"] = match.groups()[1]
             return match.groups()[0]
         return v
 
