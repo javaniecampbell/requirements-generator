74912b18d3ebd2b34a197ff33b8777d822de59b0
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 17:55:40 -0500

    refactor: added type attribute to Requirement class and category attribute to NonFunctionalRequirement class

--- domain/requirement.py
+++ domain/requirement.py
@@ -3,16 +3,21 @@ from typing import List
 
 # Functional & Non-Functional Requirements
 class Requirement:
-    def __init__(self, id: str, description: str, priority: str, status: str):
+    def __init__(
+        self, id: str, description: str, priority: str, status: str, type: str
+    ):
         self.id = id
         self.description = description
         self.priority = priority
-        self.status = status
+        self.status = status  # "Pending", "Approved", "Implemented"
+        self.type = type  # "Functional" or "Non-Functional"
 
 
 class NonFunctionalRequirement(Requirement):
     def __init__(
         self, id: str, description: str, priority: str, status: str, category: str
     ):
-        super().__init__(id, description, priority, status)
-        self.category = category
+        super().__init__(id, description, priority, status, "Non-Functional")
+        self.category = (
+            category  # "Performance", "Security", "Usability", "Reliability"
+        )
