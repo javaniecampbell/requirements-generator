6ec3b1b5a271da6a6e84a5c196f0cbd17fcfe393
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 17:57:27 -0500

    refactor: added Requirement and Acceptance Criteria functionality to UserStory class

--- domain/user_story.py
+++ domain/user_story.py
@@ -1,6 +1,8 @@
 # User Stories
 from typing import List, Optional
 
+from domain.requirement import Requirement
+
 
 class UserStory:
     def __init__(
@@ -19,9 +21,17 @@ class UserStory:
         self.as_a = as_a
         self.i_want = i_want
         self.so_that = so_that
-        self.acceptance_criteria = [] if acceptance_criteria is None else acceptance_criteria
+        self.acceptance_criteria = (
+            [] if acceptance_criteria is None else acceptance_criteria
+        )
         self.parent_feature_id = parent_feature_id
-        self.status = status
-    
-    def define_acceptance_criteria(self, criteria):
+        self.status = status  # "To Do", "In Progress", "Done"
+        self.requirements = []
+
+    def define_acceptance_criteria(self, criteria: str):
         self.acceptance_criteria.append(criteria)
+        print(f"Domain Event: Acceptance Criteria added to {self.title}")
+
+    def add_requirement(self, requirement: Requirement):
+        self.requirements.append(requirement)
+        print(f"Domain Event: {requirement.type} Requirement added to {self.title}")
