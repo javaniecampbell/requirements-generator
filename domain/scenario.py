d3804eb27a6b5a517447484c5c49743133341c3e
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 16:59:47 -0500

    feat: added Scenario class to domain package

---
+++ domain/scenario.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Scenario:
+    def __init__(
+        self, id: str, title: str, steps: List[str], parent_feature_id: str, status: str
+    ):
+        self.id = id
+        self.title = title
+        self.steps = steps
+        self.parent_feature_id = parent_feature_id
+        self.status = status
