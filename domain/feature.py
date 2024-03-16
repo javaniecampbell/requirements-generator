be793d2ed399ea6856776695c661abedae42c9e3
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 17:56:10 -0500

    feat: added user story and requirement functionality to Feature class

--- domain/feature.py
+++ domain/feature.py
@@ -1,5 +1,7 @@
 from typing import List, Optional
 
+from pkg_resources import Requirement
+
 from domain.user_story import UserStory
 
 
@@ -19,7 +21,18 @@ class Feature:
         self.description = description
         self.parent_epic_id = parent_epic_id
         self.user_stories = [] if user_stories is None else user_stories
-        self.status = status
+        self.status = status  # "To Do", "In Progress", "Done"
+        self.requirements: List["Requirement"] = []
 
     def add_user_story(self, user_story: UserStory):
+        # add parent_feature_id to user_story
         self.user_stories.append(user_story)
+        print(
+            f"Domain Event: User Story '{user_story.title}' added to Feature '{self.title}'"
+        )
+
+    def add_requirement(self, requirement: Requirement):
+        self.requirements.append(requirement)
+        print(
+            f"Domain Event: {requirement.type} Requirement added to Feature '{self.title}'"
+        )
