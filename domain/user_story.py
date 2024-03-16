97d2ae93af1e118b26233ca4680c1ef1d0d0e216
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 18:12:12 -0500

    feat: added Task and Complete Task methods to UserStory class

--- domain/user_story.py
+++ domain/user_story.py
@@ -2,6 +2,7 @@
 from typing import List, Optional
 
 from domain.requirement import Requirement
+from domain.task import Task
 
 
 class UserStory:
@@ -27,6 +28,7 @@ class UserStory:
         self.parent_feature_id = parent_feature_id
         self.status = status  # "To Do", "In Progress", "Done"
         self.requirements = []
+        self.tasks = []
 
     def define_acceptance_criteria(self, criteria: str):
         self.acceptance_criteria.append(criteria)
@@ -35,3 +37,13 @@ class UserStory:
     def add_requirement(self, requirement: Requirement):
         self.requirements.append(requirement)
         print(f"Domain Event: {requirement.type} Requirement added to {self.title}")
+
+    def add_task(self, task_description: str):
+        new_task = Task(task_description)
+        self.tasks.append(new_task)
+
+    def complete_task(self, task_index: int):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index].complete_task()
+        else:
+            raise IndexError("Task index out of bounds")
