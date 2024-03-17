c6cdf5822dc0d0d897b98b95a1763ed7c1d7d920
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 08:01:15 -0500

    feat: added helper function to rebuild aggregate from event store

---
+++ event_sourcing_sample/helpers.py
@@ -0,0 +1,10 @@
+from domain.event_store import EventStore
+from event_sourcing_sample.bank_account_aggregate import BankAccount
+
+
+def rebuild_aggregate(event_store: EventStore, aggregate_id):
+    bank_account = BankAccount(aggregate_id)
+    events = event_store.get_events_for_aggregate(aggregate_id)
+    for event in events:
+        bank_account.apply(event)
+    return bank_account
