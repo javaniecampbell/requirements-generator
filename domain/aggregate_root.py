8f2304c8b774d2dfbfc9813db0f5439925ffaf23
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 10:03:45 -0500

    feat: added versioning and change tracking to AggregateRoot class

--- domain/aggregate_root.py
+++ domain/aggregate_root.py
@@ -6,6 +6,8 @@ from domain.domain_event import DomainEvent
 class AggregateRoot:
     def __init__(self, id: str):
         self._id = id
+        self._version = 0
+        self._changes: List["DomainEvent"] = []
         self._domain_events: List["DomainEvent"] = []
 
     def add_domain_event(self, event: DomainEvent):
@@ -16,3 +18,21 @@ class AggregateRoot:
 
     def get_domain_events(self):
         return self._domain_events
+    
+    def apply(self, event: DomainEvent):
+        # self.add_domain_event(event)
+        self._changes.append(event)
+        self.apply_event(event)
+    
+    def handle(self, event: DomainEvent):
+        """This method will be overridden by derived aggregates to handle specific event types."""
+        raise NotImplementedError()
+    
+    def clear_changes(self):
+        self._changes.clear()
+
+    def load_from_history(self, events:List["DomainEvent"]):
+        for event in events:
+            self.handle(event)
+            self._version = event.version # Update the version to the event's version
+
