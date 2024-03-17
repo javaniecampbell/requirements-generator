add96053d5aeef9afdec8cb1b5617f13b994faf1
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:44:02 -0500

    docs: added documentation for the following:
    - AccountCreatedEvent class with attributes
    - and for the constructor

--- event_sourcing_sample/account_created_event.py
+++ event_sourcing_sample/account_created_event.py
@@ -2,7 +2,27 @@ from domain.event import Event
 
 
 class AccountCreated(Event):
+    """
+    Represents an event that is triggered when a new account is created.
+
+    Attributes:
+        account_id (str): The unique identifier of the account.
+        owner (str): The owner of the account.
+    """
+
     def __init__(self, account_id, owner):
+        """
+        Initializes a new instance of the AccountCreatedEvent class.
+
+        Args:
+            account_id (str): The ID of the account being created.
+            owner (str): The owner of the account.
+
+        Attributes:
+            version (int): The version of the event.
+            account_id (str): The ID of the account being created.
+            owner (str): The owner of the account.
+        """
         super().__init__(
             type="AccountCreated",
             data={"account_id": account_id, "owner": owner},
