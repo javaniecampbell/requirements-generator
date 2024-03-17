4ef5a3884e45f1739f9eb7d4987c8fb9ed254b77
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:28:46 -0500

    docs: aded docstrings and constructor comments to ProjectAggregate class

--- aggregates/project_aggregate.py
+++ aggregates/project_aggregate.py
@@ -5,7 +5,22 @@ from domain.aggregate_root import AggregateRoot
 
 
 class ProjectAggregate(AggregateRoot):
+    """
+    Represents a project aggregate.
+
+    Attributes:
+        id (str): The unique identifier of the project.
+        name (str): The name of the project.
+    """
+
     def __init__(self, id, name):
+        """
+        Initializes a new instance of the ProjectAggregate class.
+
+        Args:
+            id (int): The ID of the project.
+            name (str): The name of the project.
+        """
         super().__init__(id)
         self.name = name
         # Additional project-specific initialization
