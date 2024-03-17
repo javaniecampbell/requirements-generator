4abbf5b3d4f4f5c74fb97552119e1174dbc536bd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 09:44:05 -0500

    refactor: rebuild_aggregate function to support multiple dispatch aka function overloading

--- event_sourcing_sample/helpers.py
+++ event_sourcing_sample/helpers.py
@@ -1,11 +1,31 @@
+from typing import Optional
 from domain.event_store import EventStore
 from event_sourcing_sample.bank_account_aggregate import BankAccount
+from multipledispatch import dispatch
 
-
-def rebuild_aggregate(event_store: EventStore, aggregate_id):
+@dispatch(EventStore, str)
+def rebuild_aggregate(event_store: EventStore, aggregate_id:str):
     bank_account = BankAccount(aggregate_id)
     events = event_store.get_events_for_aggregate(aggregate_id)
     for event in events:
         print(f"Applying event: {event.type}")
         bank_account.apply(event)
     return bank_account
+
+@dispatch(EventStore, str, Optional[int])
+def rebuild_aggregate(event_store:EventStore, aggregate_id:str, upto_version:Optional[int]=None):
+    snapshot = event_store.get_latest_snapshot(aggregate_id)
+    if snapshot:
+        aggregate = snapshot.state
+        starting_version = snapshot.version
+    else:
+        aggregate = BankAccount(aggregate_id)
+        starting_version = 0
+    events = event_store.get_events_for_aggregate(
+        aggregate_id, after_version=starting_version
+    )
+    for event in events:
+        if upto_version is not None and event.version > upto_version:
+            break
+        aggregate.apply(event)
+    return aggregate
