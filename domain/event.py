02af2e86c16f65a5f904adb1ac24ca67d608b467
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 07:52:56 -0500

    feat: added aggregate_id to Event class constructor

--- domain/event.py
+++ domain/event.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from typing import List, Any
+from typing import List, Any, Optional
 
 
 class Event:
-    def __init__(self, type: str, data: Any, timestamp: datetime = datetime.now()):
+    def __init__(self,  type: str, data: Any, timestamp: datetime = datetime.now(), aggregate_id: Optional[str] = None):
         self.type = type
         self.data = data
         self.timestamp = timestamp
+        self.aggregate_id = aggregate_id
