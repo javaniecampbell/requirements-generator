007256ddad4de15cd81c55f1349abdb4bddaf881
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:30:09 -0500

    docs: added docstrings to DomainEvent class

--- domain/domain_event.py
+++ domain/domain_event.py
@@ -3,6 +3,21 @@ from typing import Any
 
 
 class DomainEvent:
+    """
+    Represents a domain event.
+
+    Attributes:
+        name (str): The name of the domain event.
+        data (Any): The data associated with the domain event.
+    """
+
     def __init__(self, name: str, data: Any):
+        """
+        Initializes a new instance of the DomainEvent class.
+
+        Args:
+            name (str): The name of the domain event.
+            data (Any): The data associated with the domain event.
+        """
         self.name = name
         self.data = data
