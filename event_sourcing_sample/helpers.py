7002377b1bddd936f6312784aee99c659e4173bb
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 13:07:48 -0500

    fix: rebuild_aggregate function signature
    - remove typings no currently supported [PR #72](https://github.com/mrocklin/multipledispatch/issues/72)

--- event_sourcing_sample/helpers.py
+++ event_sourcing_sample/helpers.py
@@ -3,8 +3,9 @@ from domain.event_store import EventStore
 from event_sourcing_sample.bank_account_aggregate import BankAccount
 from multipledispatch import dispatch
 
+
 @dispatch(EventStore, str)
-def rebuild_aggregate(event_store: EventStore, aggregate_id:str):
+def rebuild_aggregate(event_store: EventStore, aggregate_id: str):
     bank_account = BankAccount(aggregate_id)
     events = event_store.get_events_for_aggregate(aggregate_id)
     for event in events:
@@ -12,8 +13,11 @@ def rebuild_aggregate(event_store: EventStore, aggregate_id:str):
         bank_account.apply(event)
     return bank_account
 
-@dispatch(EventStore, str, Optional[int])
-def rebuild_aggregate(event_store:EventStore, aggregate_id:str, upto_version:Optional[int]=None):
+
+@dispatch(EventStore, str, int)
+def rebuild_aggregate(
+    event_store: EventStore, aggregate_id: str, upto_version: Optional[int] = None
+):
     snapshot = event_store.get_latest_snapshot(aggregate_id)
     if snapshot:
         aggregate = snapshot.state
