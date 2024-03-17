d2bf51449cc3656649c6150c6c17ca3a51c463cd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:40:12 -0500

    docs: added docstrings for Snapshot class

--- domain/snapshot.py
+++ domain/snapshot.py
@@ -1,5 +1,22 @@
 class Snapshot:
+    """
+    Represents a snapshot of the state of an aggregate.
+
+    Attributes:
+        aggregate_id (str): The unique identifier of the aggregate.
+        version (int): The version number of the snapshot.
+        state: The state of the aggregate at the time of the snapshot.
+    """
+
     def __init__(self, aggregate_id: str, version: int, state):
+        """
+        Initializes a new Snapshot object.
+
+        Args:
+            aggregate_id (str): The ID of the aggregate.
+            version (int): The version number of the snapshot.
+            state: The state of the aggregate at the time of the snapshot.
+        """
         self.aggregate_id = aggregate_id
         self.version = version
         self.state = state
