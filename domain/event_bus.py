b6890ce30a25aa5db7ce94bcdc2c2a822ee989bb
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:31:18 -0500

    docs: added docstrings to event bus implementation
    - added docstrings to subscribe and publish events methods

--- domain/event_bus.py
+++ domain/event_bus.py
@@ -4,13 +4,31 @@ from domain.domain_event import DomainEvent
 
 
 class EventBus:
+    """
+    A simple event bus implementation that allows subscribing to and publishing events.
+    """
+
     _subscribers: List[Callable] = []
 
     @classmethod
     def subscribe(cls, event_handler: Callable):
+        """
+        Subscribe a callable event handler to the event bus.
+
+        Args:
+            event_handler (Callable): The event handler function to be subscribed.
+
+        """
         cls._subscribers.append(event_handler)
 
     @classmethod
     def publish(cls, event: DomainEvent):
+        """
+        Publish an event to all the subscribed event handlers.
+
+        Args:
+            event (DomainEvent): The event to be published.
+
+        """
         for handler in cls._subscribers:
             handler(event)
