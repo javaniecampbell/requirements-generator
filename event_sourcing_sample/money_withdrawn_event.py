55f7d1b5c5f2648efcb4b223b49cba92fefede35
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:49:00 -0500

    docs: added docstring to MoneyWithdrawn class

--- event_sourcing_sample/money_withdrawn_event.py
+++ event_sourcing_sample/money_withdrawn_event.py
@@ -2,7 +2,27 @@ from domain.event import Event
 
 
 class MoneyWithdrawn(Event):
+    """
+    Represents an event indicating that money has been withdrawn from an account.
+
+    Attributes:
+        account_id (str): The ID of the account from which the money was withdrawn.
+        amount (float): The amount of money that was withdrawn.
+    """
+
     def __init__(self, account_id, amount):
+        """
+        Initializes a new instance of the MoneyWithdrawnEvent class.
+
+        Args:
+            account_id (str): The ID of the account from which money is being withdrawn.
+            amount (float): The amount of money being withdrawn.
+
+        Attributes:
+            version (int): The version of the event.
+            account_id (str): The ID of the account from which money is being withdrawn.
+            amount (float): The amount of money being withdrawn.
+        """
         super().__init__(
             type="MoneyWithdrawn",
             data={"account_id": account_id, "amount": amount},
