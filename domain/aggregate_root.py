79b80882ec1343209019e98c18281d8db5c87cf2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 10:41:07 -0500

    refactor: AggregateRoot class to include snapshot functionality

--- domain/aggregate_root.py
+++ domain/aggregate_root.py
@@ -21,18 +21,29 @@ class AggregateRoot:
     
     def apply(self, event: DomainEvent):
         # self.add_domain_event(event)
+        # self._apply_event(event)
+        self.handle(event)
+        self._version = event.version # Update version based on event version
         self._changes.append(event)
-        self.apply_event(event)
     
     def handle(self, event: DomainEvent):
         """This method will be overridden by derived aggregates to handle specific event types."""
         raise NotImplementedError()
+    
+    def snapshot_state(self):
+        """Return the current state as a snapshot."""
+        raise NotImplementedError
+
+    def restore_from_snapshot(self):
+        """Restore the state from a snapshot."""
+        raise NotImplementedError
     
     def clear_changes(self):
         self._changes.clear()
 
     def load_from_history(self, events:List["DomainEvent"]):
         for event in events:
-            self.handle(event)
-            self._version = event.version # Update the version to the event's version
+            # self.handle(event)
+            # self._version = event.version # Update the version to the event's version
+            self.apply(event)
 
