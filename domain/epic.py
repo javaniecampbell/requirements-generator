aed862744eb6a6d755232905125f97e0294fe421
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:30:21 -0500

    docs: added docstrings and comments to Epic class

--- domain/epic.py
+++ domain/epic.py
@@ -7,6 +7,10 @@ from domain.feature import Feature
 
 
 class Epic:
+    """
+    Represents an epic, which is a large user story or a collection of related user stories.
+    """
+
     def __init__(
         self,
         id: str,
@@ -15,6 +19,16 @@ class Epic:
         features: Optional[List["Feature"]],
         status: str,
     ):
+        """
+        Initializes a new instance of the Epic class.
+
+        Args:
+            id (str): The unique identifier of the epic.
+            title (str): The title of the epic.
+            description (str): The description of the epic.
+            features (Optional[List[Feature]]): The list of features associated with the epic.
+            status (str): The status of the epic. Can be one of "To Do", "In Progress", or "Done".
+        """
         self.id = id
         self.title = title
         self.description = description
@@ -22,6 +36,12 @@ class Epic:
         self.status = status  # "To Do", "In Progress", "Done"
 
     def add_feature(self, feature: Feature):
+        """
+        Adds a feature to the epic.
+
+        Args:
+            feature (Feature): The feature to be added.
+        """
         self.features.append(feature)
         #  considering moving into a separate aggregate class called EpicAggregate
         EventBus.publish(
