b2f6127df833e94a242f751c2834c32e23fc4a60
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-16 20:21:14 -0500

    refactor: Project class constructor to include an id parameter
    - added epics list

--- domain/project.py
+++ domain/project.py
@@ -1,3 +1,10 @@
+from typing import List
+from domain.epic import Epic
+
+
+# domain models should contain the business logic without domain events being called
 class Project:
-    def __init__(self, name: str):
+    def __init__(self, id: str, name: str):
+        self.id = id
         self.name = name
+        self.epics: List["Epic"] = []
