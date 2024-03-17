d4e1aa1957766c3c9670ee005baa3ddf4e3e6923
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:46:03 -0500

    docs: added documentation for the following:
    - attributes to MoneyDepositedEvent class

--- event_sourcing_sample/money_deposited_event.py
+++ event_sourcing_sample/money_deposited_event.py
@@ -2,7 +2,27 @@ from domain.event import Event
 
 
 class MoneyDeposited(Event):
+    """
+    Represents an event that is triggered when money is deposited into an account.
+
+    Attributes:
+        account_id (str): The ID of the account where the money is deposited.
+        amount (float): The amount of money deposited.
+    """
+
     def __init__(self, account_id, amount):
+        """
+        Initializes a MoneyDepositedEvent object.
+
+        Args:
+            account_id (str): The ID of the account where the money is being deposited.
+            amount (float): The amount of money being deposited.
+
+        Attributes:
+            version (int): The version of the event.
+            account_id (str): The ID of the account where the money is being deposited.
+            amount (float): The amount of money being deposited.
+        """
         super().__init__(
             type="MoneyDeposited",
             data={"account_id": account_id, "amount": amount},
