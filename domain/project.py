60e594185676fcb01de84683da126bd28c70ee84
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-17 14:37:09 -0500

    docs: added docstrings to Project class & constructor

--- domain/project.py
+++ domain/project.py
@@ -3,8 +3,31 @@ from domain.epic import Epic
 
 
 # domain models should contain the business logic without domain events being called
+
+
 class Project:
+    """
+    Represents a project.
+
+    Attributes:
+        id (str): The unique identifier of the project.
+        name (str): The name of the project.
+        epics (List[Epic]): The list of epics associated with the project.
+    """
+
     def __init__(self, id: str, name: str):
+        """
+        Initializes a new Project instance.
+
+        Args:
+            id (str): The unique identifier for the project.
+            name (str): The name of the project.
+
+        Attributes:
+            id (str): The unique identifier for the project.
+            name (str): The name of the project.
+            epics (List[Epic]): The list of epics associated with the project.
+        """
         self.id = id
         self.name = name
         self.epics: List["Epic"] = []
