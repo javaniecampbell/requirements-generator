2c52d03bd9005c368d06bd09bb9886def1bbcc8c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-18 00:29:10 -0500

    chore: cleaned up duplicate artifacts

--- domain/event.py
+++ domain/event.py
@@ -5,6 +5,7 @@ from typing import List, Any, Optional
 from typing import Any, Optional
 from datetime import datetime
 
+
 class Event:
     """
     Represents an event in the system.
@@ -17,25 +18,24 @@ class Event:
         version (Optional[int]): The version of the event. Defaults to None.
     """
 
-    class Event:
-        def __init__(
-            self,
-            type: str,
-            data: Any,
-            timestamp: datetime = datetime.now(),
-            aggregate_id: Optional[str] = None,
-        ):
-            """
-            Initialize a new Event object.
+    def __init__(
+        self,
+        type: str,
+        data: Any,
+        timestamp: datetime = datetime.now(),
+        aggregate_id: Optional[str] = None,
+    ):
+        """
+        Initialize a new Event object.
 
-            Args:
-                type (str): The type of the event.
-                data (Any): The data associated with the event.
-                timestamp (datetime, optional): The timestamp of the event. Defaults to the current datetime.
-                aggregate_id (str, optional): The ID of the aggregate associated with the event. Defaults to None.
-            """
-            self.type = type
-            self.data = data
-            self.version: Optional[int] = None
-            self.timestamp = timestamp
-            self.aggregate_id = aggregate_id
+        Args:
+            type (str): The type of the event.
+            data (Any): The data associated with the event.
+            timestamp (datetime, optional): The timestamp of the event. Defaults to the current datetime.
+            aggregate_id (str, optional): The ID of the aggregate associated with the event. Defaults to None.
+        """
+        self.type = type
+        self.data = data
+        self.version: Optional[int] = None
+        self.timestamp = timestamp
+        self.aggregate_id = aggregate_id
