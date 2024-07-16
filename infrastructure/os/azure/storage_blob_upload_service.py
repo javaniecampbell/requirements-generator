b8d0eb8816225a6c58212e306c355244a9c8ef91
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-16 18:01:51 -0500

    refactor: Update StorageBlobUploadService to support uploading blobs to different containers
    This commit updates the `StorageBlobUploadService` class in the `storage_blob_upload_service.py` file to support uploading blobs to different containers in Azure Blob Storage. It adds a new parameter `container_name` to the `upload_blob` method, allowing the user to specify the name of the container where the blob will be uploaded. This change improves the flexibility and configurability of the codebase, enabling the uploading of blobs to specific containers based on requirements.

--- infrastructure/os/azure/storage_blob_upload_service.py
+++ infrastructure/os/azure/storage_blob_upload_service.py
@@ -8,12 +8,33 @@ container_name = "your_container_name"
 
 
 class StorageBlobUploadService(BlobUploadService):
+    """
+    A class that provides methods to upload blobs to Azure Blob Storage.
+    """
+
     def __init__(self, connection_string):
+        """
+        Initializes a new instance of the StorageBlobUploadService class.
+
+        Args:
+            connection_string (str): The connection string for the Azure Blob Storage account.
+        """
         self.blob_service_client = BlobServiceClient.from_connection_string(
             connection_string
         )
 
     def upload_blob(self, container_name, blob_name, data) -> str:
+        """
+        Uploads a blob to the specified container in Azure Blob Storage.
+
+        Args:
+            container_name (str): The name of the container.
+            blob_name (str): The name of the blob.
+            data (bytes): The data to be uploaded.
+
+        Returns:
+            str: The URL of the uploaded blob.
+        """
         # If the container does not exist, create a new container with the name specified
         container_client = self.blob_service_client.get_container_client(container_name)
         if not container_client.exists():
@@ -27,6 +48,16 @@ class StorageBlobUploadService(BlobUploadService):
         return blob_client.url
 
     def upload_blob(self, blob_name, data) -> str:
+        """
+        Uploads a blob to the default container in Azure Blob Storage.
+
+        Args:
+            blob_name (str): The name of the blob.
+            data (bytes): The data to be uploaded.
+
+        Returns:
+            str: The URL of the uploaded blob.
+        """
         blob_client = self.blob_service_client.get_blob_client(
             container=container_name, blob=blob_name
         )
