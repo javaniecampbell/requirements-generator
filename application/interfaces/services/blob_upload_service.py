16572264c69e7fae47514ff41eaf648c602a885c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-16 17:59:58 -0500

    refactor: Update BlobUploadService interface to include container name parameter
    This commit updates the `BlobUploadService` interface in the `blob_upload_service.py` file to include a `container_name` parameter in the `upload_blob` method. This change allows for specifying the name of the container where the blob will be uploaded. It improves the flexibility and configurability of the codebase by providing a way to upload blobs to different containers within the storage service.

--- application/interfaces/services/blob_upload_service.py
+++ application/interfaces/services/blob_upload_service.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class BlobUploadService(ABC):
@@ -6,6 +6,7 @@ class BlobUploadService(ABC):
     Interface for uploading blobs to a storage service.
     """
 
+    @abstractmethod
     def upload_blob(self, blob_name: str, blob_data: bytes) -> str:
         """
         Uploads a blob to a storage service.
@@ -17,4 +18,19 @@ class BlobUploadService(ABC):
         Returns:
             str: The URL of the uploaded blob.
         """
-        pass
+        ...
+
+    @abstractmethod
+    def upload_blob(self, container_name: str, blob_name: str, blob_data: bytes) -> str:
+        """
+        Uploads a blob to a storage service.
+
+        Args:
+            container_name (str): The name of the container.
+            blob_name (str): The name of the blob.
+            blob_data (bytes): The data of the blob.
+
+        Returns:
+            str: The URL of the uploaded blob.
+        """
+        ...
